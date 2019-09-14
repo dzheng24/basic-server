9500826473e0bb3765f9ea3f6e0d78b3a3d11dbd
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-09-13 17:08:08 -0700

    building backend

---
+++ public/README.md
@@ -0,0 +1,11 @@
+# portfolio
+
+Number and name of feature: __________Refactor the CSS______________________
+
+Estimate of time needed to complete: __1 hour__
+
+Start time: __11:55 am (just before lunch)___
+
+Finish time: _stopped at 1:41 pm____
+
+Actual time needed to complete: ___used about 40 minutes (not yet finished)_
---
+++ server.js
@@ -0,0 +1,38 @@
+'use stric';
+
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.static('public'));
+
+
+const PORT = process.env.PORT || 3000;
+
+// routes
+// below is home route
+// app.get('/', (request, response) => {
+//     response.redirect('index.html');
+// })
+
+app.get('/hello', (request, response) => {
+    response.send('Hello');
+})
+
+app.get('/data', (request, response) => {
+    let chris = {
+        name: 'Chris',
+        hair: 'dark brown',
+        eyes: 'brown',
+        favoriteFood: 'curry',
+        favoriteDrink: 'beer',
+    }
+
+    response.status(200).json(chris);
+})
+
+app.use('*', (request, response) => {
+    response.status(404).send('Sorry, that route does not exist');
+})
+
+app.listen((PORT), () => console.log(`listening on ${PORT}`));
